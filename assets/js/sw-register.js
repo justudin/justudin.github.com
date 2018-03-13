968f816fa69f4dd5d8f38f0a210b64b4c69700b4
M. Syafrudin <just.udin@yahoo.com>
2018-03-13 12:36:30 +0900

    update sw-register

--- assets/js/sw-register.js
+++ assets/js/sw-register.js
@@ -6,7 +6,7 @@ if (navigator.serviceWorker.controller) {
 } else {
 
 //Register the ServiceWorker
-  navigator.serviceWorker.register('sworkers.js', {
+  navigator.serviceWorker.register('/sworkers.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
